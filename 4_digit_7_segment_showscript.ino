77a31c978412c52043a6da7108fc149b50ce5a7e
YassineHane <yassine.hane@gmail.com>
2022-11-24 21:49:35 +0100

    Adding doc mapping 7segment and start sketch

---
+++ 4_digit_7_segment_showscript.ino
@@ -0,0 +1,16 @@
+
+
+// let's define the pin we use for the ESP32 in accordance with the schematics in 4Digit7Segment_mapping.jpg file
+
+#define segA 19
+#define segB 15
+#define segC 27
+#define segD 32
+#define segE 34
+#define segF 5
+#define segG 12
+#define D1 21
+#define D2 16
+#define D3 0
+#define D4 13
+
